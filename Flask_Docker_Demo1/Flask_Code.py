612d90c55e0f93a87fa96449d1b794edb8251567
akku2311 <140010116021@adit.ac.in>
2019-09-07 14:19:50 +0530

    First Change in Branch 2

--- Flask_Docker_Demo1/Flask_Code.py
+++ Flask_Docker_Demo1/Flask_Code.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # first endpoint #
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized - 2'
+    return 'Flask Dockerized Master 1'
 
 
 if __name__ == '__main__':
