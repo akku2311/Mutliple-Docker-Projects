68c158ddca3932922c1cf2c9aa7ae98ad45902d8
akku2311 <140010116021@adit.ac.in>
2019-09-07 13:20:35 +0530

    Initial commit

---
+++ Flask_Docker_Demo2/Flask_Code.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# first endpoint #
+@app.route('/')
+def hello_world():
+    return 'Flask Dockerized2'
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='127.0.0.1', port=5020)
