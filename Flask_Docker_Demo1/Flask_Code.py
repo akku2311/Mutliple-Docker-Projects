1804800916913d43152241bbf71927f09836fb00
akku2311 <140010116021@adit.ac.in>
2019-09-08 21:08:49 +0530

    Master Branch Commit before review1

--- Flask_Docker_Demo1/Flask_Code.py
+++ Flask_Docker_Demo1/Flask_Code.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # first endpoint #
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized - 1'
+    return 'Flask Dockerized - 11'
 
 if __name__ == '__main__':
     app.run(debug=False, host='127.0.0.1', port=5010)
