2ff521c897ff0e3a5c159d9b68aca8ed8d9aff6c
hardikpatel172 <hardikpatel172@gmail.com>
2019-09-09 15:33:26 +0530

    Test commit on Demo 2 project with Demo 2 Branch

--- Flask_Docker_Demo2/Flask_Code.py
+++ Flask_Docker_Demo2/Flask_Code.py
@@ -11,8 +11,8 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    # Changes done by Hardik Starts
+    # Changes done by Hardik Starts through Check out of Branch 2
     print("Changes done by Hardik with Akash_Docker_Branch_2")
-    # Changes done by Hardik Ends
+    # Changes done by Hardik Ends through Check out of Branch 2
 
     app.run(debug=False, host='127.0.0.1', port=5020)
